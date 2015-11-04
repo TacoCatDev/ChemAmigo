3f4ba4a97e716a0ff59803f3532a224a1a6e3873
Ethan R. Jamieson <s16600609@dvh-saplk42.local>
2015-11-04 14:25:01 -0700

    added more stuff

--- ChemAmigo/ViewController.swift
+++ ChemAmigo/ViewController.swift
@@ -18,6 +18,10 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+        
+        
+        
+        //poooooooppp
     }
 
 
